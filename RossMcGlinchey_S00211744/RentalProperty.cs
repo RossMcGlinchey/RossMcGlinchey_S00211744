72d05252a8107113194400ccadc674acd6f06185
Ross Mc Glinchey <S00211744@mail.itsligo.ie>
2022-05-17 09:37:53 +0100

    Created class and added properties

---
+++ RossMcGlinchey_S00211744/RentalProperty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RossMcGlinchey_S00211744
+{
+    class RentalProperty
+    {
+        //Properties
+        public int ID { get; set; }
+        public enum RentalType { House, Flat, Share }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+
+        //Constructor
+
+        //Methods
+        public void IncreaseRent(decimal percentIncrease)
+        {
+            Price = Price + (Price * percentIncrease / 100);
+        }
+    }
+}
